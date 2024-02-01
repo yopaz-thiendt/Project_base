fa271955c660ae6e6b9613d9f76b772e0977f355
yopaz-thiendt <88067938+yopaz-tiendv@users.noreply.github.com>
2024-02-01 15:17:28 +0700

    Task 24738: Integrate base network and provider

---
+++ lib/api/auth/login.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:project_base/models/api/api_response_model.dart';
+import 'package:project_base/models/api/auth_login_response_model.dart';
+import 'package:project_base/utils/constants.dart';
+import 'package:project_base/utils/network_helper.dart';
+
+class AuthLoginApi {
+  late NetworkHelper helper;
+  Map<String, String>? queryParameters = {};
+
+  AuthLoginApi(
+    this.queryParameters,
+  ) {
+    helper = NetworkHelper(
+      endPoint: Constants.loginPath,
+      queryParameters: queryParameters,
+      accessToken: '',
+    );
+  }
+
+  Future<AuthLoginResponseModel> login(String body) async {
+    var res = await helper.callPost(body);
+    return generateAuthLogin(res);
+  }
+}
+
+AuthLoginResponseModel generateAuthLogin(APIResponseModel res) {
+  if (res.statusCode == HttpStatus.ok) {
+    return AuthLoginResponseModel(
+      res.data['access_token'].toString(),
+      res.message,
+      res.statusCode,
+    );
+  } else {
+    return AuthLoginResponseModel(
+      "",
+      res.message,
+      res.statusCode,
+    );
+  }
+}
